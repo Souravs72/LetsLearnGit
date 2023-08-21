a466f672bf5511b9625b0bcef675cf9392a8fe99
Sourav Singh <souravsingh2609@gmail.com>
2023-08-21 11:11:03 +0530

    addded Multily.java

--- Multiply.java
+++ Multiply.java
@@ -2,7 +2,7 @@ public class Sum {
     public static void main(String args[]) {
         int a = 5; 
         int b = 3;
-        int sum = a + b;
+        int sum = a * b;
         System.out.println("The sum is "+sum);
     }
 }
